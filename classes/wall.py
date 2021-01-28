e66c1edd663e580cc10614c777b852b945c806ef
Arkadiusz Czader <czader@student.agh.edu.pl>
2021-01-28 22:42:37 +0100

    Add simple Wall class

---
+++ classes/wall.py
@@ -0,0 +1,14 @@
+from classes.game_object import GameObject
+import pygame
+
+
+class Wall(GameObject):
+    """It's just a wall"""
+
+    wall_sprite = None
+
+    def __init__(self, x: int, y: int):
+        # Load sprite only once
+        if Wall.wall_sprite is None:
+            Wall.wall_sprite = pygame.image.load('img/wall.png').convert_alpha()
+        super().__init__(x, y, Wall.wall_sprite)
